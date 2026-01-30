eaaf178c39c2fcdcfff7871a7af8130a84079916
astra-the-boop <astra.the.boop@icloud.com>
2026-01-31 01:40:16 +0700

    we didn't start the fire it was always burning since the worlds been turning we didn't start the fire no we didnt light it but we tried to fight it

--- public/script.js
+++ public/script.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function(){
     fetch("/content").then(res => res.json()).then(
         data => {
             document.getElementById("title").textContent = data.title;
-            document.getElementById("body").innerHTML = data.body;
-            document.getElementById("date").innerHTML = data.date;
+            document.getElementById("body").innerHTML = marked.parse(data.body);
+            document.getElementById("date").innerHTML = Intl.DateTimeFormat("en-US", {year:"numeric", month:"long", day:"numeric"}).format(new Date(data.date));
             document.getElementById("publisher").innerHTML = data.publisher;
             document.title = data.title;
         }
