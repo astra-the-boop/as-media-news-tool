ba8b1a22217bcd71285c5121fcb18f44f412ae4c
astra-the-boop <astra.the.boop@icloud.com>
2026-01-31 22:31:19 +0700

    i'm too tired for this shit

--- express.js
+++ express.js
@@ -1,36 +1,57 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
-const port = 3298;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-const dataFile = path.join(__dirname, './data.json');
+const port = 3299;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+    if (req.path.endsWith("editor.html")) {
+        return res.status(403).send("Forbidden");
+    }
+    next();
+});
+
 app.use(express.static("public"));
 
-function read(){
-    return JSON.parse(fs.readFileSync(dataFile, 'utf8'));
+const dataFile = path.join(__dirname, "data.json");
+
+function read() {
+    if (!fs.existsSync(dataFile)) {
+        return {};
+    }
+    return JSON.parse(fs.readFileSync(dataFile, "utf8"));
 }
 
-function write(data){
-    fs.writeFileSync(dataFile,JSON.stringify(data, null, 2));
+function write(data) {
+    fs.writeFileSync(dataFile, JSON.stringify(data, null, 2));
 }
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/page.html"));
+});
+
+app.get("/page", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/page.html"));
+});
+
+app.get("/editor", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/editor.html"));
+});
 
 app.post("/submit", (req, res) => {
-    const {title, body, date, publisher, writer, subtext, location} = req.body;
-    write({title, body, date, publisher, writer, subtext, location});
-    res.redirect("/page.html")
-})
+    const { title, body, date, publisher, writer, subtext, location } = req.body;
+    write({ title, body, date, publisher, writer, subtext, location });
+    res.redirect("/page");
+});
 
 app.get("/content", (req, res) => {
     res.json(read());
-})
+});
 
 app.listen(port, () => {
-    console.log("server started on port", port);
-})
+    console.log(`server started on http://localhost:${port}`);
+});
