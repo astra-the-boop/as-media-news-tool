3f3f7b0a224b97913eb62c14cf37b2c5bced0fc4
astra-the-boop <astra.the.boop@icloud.com>
2026-01-31 17:00:30 +0700

    autofil

--- public/theotherfuckingone.js
+++ public/theotherfuckingone.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch("/content").then(res => res.json()).then(
         data => {
             document.getElementById("publisher").value = data.publisher;
+            document.getElementById("writer").value = data.writer;
+            document.getElementById("subtext").value = data.subtext;
             document.getElementById("title").value = data.title;
             document.getElementById("date").value = data.date;
             document.getElementById("body").value = data.body;
